aeefcc36a1894adfd8a7d80e4688cd442a2fb937
Francesco Delmonaco <francescodelmonaco1999@gmail.com>
2025-12-03 17:56:08 +0100

    fix nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,9 @@ export default function Navbar({ activeSection, setActiveSection }) {
             initial={{ x: -100, opacity: 0 }}
             animate={{ x: 0, opacity: 1 }}
             transition={{ duration: 0.5 }}
-            className="h-[7%] lg:h-[50%] w-[90%] md:w-[60%] lg:w-[7%] xl:w-[5%] self-center flex justify-center relative rounded-full mx-12 my-6 lg:my-0 lg:py-12"
+            className="h-[7%] lg:h-[50%] w-[75%] md:w-[60%] lg:w-[7%] xl:w-[5%] self-center flex justify-center relative rounded-full mx-12 my-6 lg:my-0 lg:py-12"
         >
-            <div className="z-10 flex lg:flex-col w-full h-full items-center self-center justify-between relative mx-2 lg:mx-0 transparent-card rounded-full">
+            <div className="z-10 flex lg:flex-col w-full h-full items-center self-center justify-between relative transparent-card rounded-full p-2">
                 {menuItems.map((item, index) => {
                     const Icon = item.icon;
                     const isActive = activeSection === item.id;
@@ -29,7 +29,7 @@ export default function Navbar({ activeSection, setActiveSection }) {
                             {isActive && (
                                 <motion.div
                                     layoutId="activeBackground"
-                                    className="absolute inset-0 bg-white/15 rounded-full m-2"
+                                    className="absolute inset-1 bg-white/15 rounded-full"
                                     transition={{
                                         type: "spring",
                                         stiffness: 380,
@@ -37,9 +37,9 @@ export default function Navbar({ activeSection, setActiveSection }) {
                                     }}
                                 />
                             )}
-                            <div className="p-5 relative z-10">
+                            <div className="p-3 xl:p-3 relative z-10">
                                 <Icon
-                                    className={`w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 xl:w-12 xl:h-12 ${isActive ? 'text-white' : 'text-gray-400'} group-hover:text-white transition-colors`}
+                                    className={`w-6 h-6 sm:w-6 sm:h-6 md:w-8 md:h-8 xl:w-10 xl:h-10 ${isActive ? 'text-white' : 'text-gray-400'} group-hover:text-white transition-colors`}
                                 />
                             </div>
                         </motion.button>
